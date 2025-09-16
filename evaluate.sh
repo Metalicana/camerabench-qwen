9aa85dfa6956b85df6b3d43df7b76ad3281eaa28
Radi <alradi9923@gmail.com>
2025-09-16 19:04:44 -0400

    new eval script

---
+++ evaluate.sh
@@ -0,0 +1,4 @@
+python evaluate_camerabench_qwen.py \
+ --model Qwen/Qwen2.5-VL-7B-Instruct \
+ --split test \
+ --local_data_root /lustre/fs1/home/ab575577/projects_fall_2025/t2v_metrics/camerabench/data
---
+++ evaluate_crcv.sh
@@ -0,0 +1,5 @@
+python evaluate_camerabench_qwen.py \
+ --model Qwen/Qwen2.5-VL-7B-Instruct \
+ --split test \
+ --local_data_root /home/ab575577/projects_fall_2025/t2v_metrics/camerabench/data
+
