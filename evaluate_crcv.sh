a21c473906a3ac73f07af3d4b3ee08caaafb2b09
Abdul Mohaimen Al Radi <alradu9923@gmail.com>
2025-09-18 09:24:43 -0400

    update script

--- evaluate_crcv.sh
+++ evaluate_crcv.sh
@@ -1,5 +1,6 @@
 python evaluate_camerabench_qwen.py \
- --model Qwen/Qwen2.5-VL-7B-Instruct \
- --split test \
- --local_data_root /home/ab575577/projects_fall_2025/t2v_metrics/camerabench/data
-
+  --model Qwen/Qwen2.5-VL-7B-Instruct \
+  --split test \
+  --local_data_root /home/ab575577/projects_fall_2025/t2v_metrics/camerabench/data \
+  --out_file camerabench_summary.csv \
+  --raw_file camerabench_raw_scores.csv
